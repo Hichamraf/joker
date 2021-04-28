6c3f221cb58d0e8c24bc4311f48cf7b33ef57842
hicham <rafia.hicham@nestle.com>
2021-04-28 22:29:31 +0000

    Add data layer.

---
+++ app/src/main/java/com/example/joker/data/JokesApi.kt
@@ -0,0 +1,10 @@
+package com.example.joker.data
+
+import com.example.joker.model.Joke
+import retrofit2.http.GET
+
+interface JokesApi {
+
+@GET("random_ten")
+suspend fun getJokes() : List<Joke>
+}
